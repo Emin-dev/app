d54d1caf772b4be4bf28aa7f77a446531afe06fe
Emin <ismemin9@gmail.com>
2023-01-27 23:41:48 +0000

    r

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import { useState } from "react";
+import Home from './Home';
 
 function App() {
 
-  const [a,seta] = useState(0)
+  const [a, seta] = useState(1)
 
   return (
     <div className="app">
+      <Home />
       <h1>{a}</h1>
-      <button onClick={() => seta((t)=>t+1)}>+</button>
-      <button onClick={() => seta((t)=>t-1)}>-</button>
-
-
-
-
-      
+      <button onClick={() => seta((t) => t * 10)}>+</button>
+      <button onClick={() => seta((t) => t - 100)}>-</button>
     </div>
   );
 }
+
+
 
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+	return (
+		<div>
+			<p>Welcome to Home</p>
+		</div>
+	)
+}
+
+export default Home;
+
